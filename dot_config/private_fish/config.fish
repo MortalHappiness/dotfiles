801e95e2bcf4cd9fd5fcf5f7b28dd963cba85bac
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-02-04 13:17:25 +0800

    Update fzf config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -59,12 +59,10 @@ end
 # External tools
 
 # fzf
-if type -q fzf
-  set -x FZF_DEFAULT_OPTS '
-    --layout=reverse
-    --color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672
-  '
-end
+set -x FZF_DEFAULT_OPTS '
+  --layout=reverse
+  --color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672
+'
 
 # fzf.fish
 if functions -q fzf_configure_bindings
